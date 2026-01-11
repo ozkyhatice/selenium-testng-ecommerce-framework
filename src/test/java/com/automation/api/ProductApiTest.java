5dc1c8bd59017c20405008d345cdadb99e74455e
Hatice Özkaya <ozkyyhatice@gmail.com>
2026-01-11 17:23:47 +0300

    Add ProductApiTest: Implement API test for product verification with status and attribute checks

---
+++ src/test/java/com/automation/api/ProductApiTest.java
@@ -0,0 +1,36 @@
+package com.automation.api;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.testng.asserts.SoftAssert;
+
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.List;
+
+public class ProductApiTest {
+    @Test
+    public void verifyAllProductsTest() {
+        SoftAssert softAssert = new SoftAssert();
+        RestAssured.baseURI = "https://automationexercise.com/api";
+        Response response = get("/productsList");
+        JsonPath jsonPath = response.jsonPath();
+        //status code "200"
+        softAssert.assertEquals(response.getStatusCode(), 200, "Status code is not 200");
+        //verify that product list is not empty
+        List<Object> products = jsonPath.getList("products");
+        softAssert.assertEquals(products.size() > 0, true, "Product list is empty");
+        String firstProductName = jsonPath.getString("products[0].name");
+        softAssert.assertNotNull(firstProductName, "First product name is null");
+        String firstProductPrice = jsonPath.getString("products[0].price");
+        softAssert.assertNotNull(firstProductPrice, "First product price is null");
+        //price must contain Rs
+        softAssert.assertTrue(firstProductPrice.contains("Rs"), "First product price does not contain 'Rs'");
+        String firstProductCategory = jsonPath.getString("products[0].category.category");
+        softAssert.assertNotNull(firstProductCategory, "First product category is null");
+        softAssert.assertAll();
+    }
+}
