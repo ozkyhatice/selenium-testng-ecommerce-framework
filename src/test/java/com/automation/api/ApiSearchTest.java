828f224838d4ce4c5197e0e68f03bf7dedc707fd
Hatice Özkaya <ozkyyhatice@gmail.com>
2026-01-15 15:40:20 +0300

    Add logging dependencies and configuration; enhance ApiSearchTest with logging

--- src/test/java/com/automation/api/ApiSearchTest.java
+++ src/test/java/com/automation/api/ApiSearchTest.java
@@ -7,8 +7,12 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import io.restassured.response.Response;
 import io.restassured.path.json.JsonPath;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ApiSearchTest extends ApiBaseTest {
+
+    private static final Logger logger = LogManager.getLogger(ProductApiTest.class);
     @DataProvider(name = "searchData")
     public Object[][] getSearchData() {
         return new Object[][] {
@@ -19,6 +23,7 @@ public class ApiSearchTest extends ApiBaseTest {
     }
     @Test(dataProvider = "searchData")
     public void verifySearchWithParameter(String product, int expectedStatusCode) {
+        logger.info("Starting test for product search: " + product);
         SoftAssert softAssert = new SoftAssert();
         Response response = given()
                 .spec(requestSpec)
@@ -26,11 +31,13 @@ public class ApiSearchTest extends ApiBaseTest {
                 .when()
                 .post("/searchProduct");
         JsonPath jsonPath = response.jsonPath();
+        logger.info("Response received: " + response.asString());
         //assertion
         softAssert.assertEquals(response.getStatusCode(), 200, "Incorrect status code");
         softAssert.assertEquals(jsonPath.getInt("responseCode"), expectedStatusCode, "Status code does not match");
         String firstProduct = jsonPath.getString("products[0].name").toLowerCase();
         softAssert.assertTrue(firstProduct.contains(product.toLowerCase()), "First product does not match search term");
+        logger.info("Assertions completed for product search: " + product);
         softAssert.assertAll();
     }
 
